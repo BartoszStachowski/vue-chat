e90286846c7071462ee4dcba32c6979a1d3cc628
bstachowski <bstachowski@oferteo.pl>
2024-11-21 21:18:39 +0100

    removal of next from the routing

--- src/router/index.js
+++ src/router/index.js
@@ -4,30 +4,29 @@ import ChatroomView from '@/views/ChatroomView.vue';
 import { auth } from '@/firebase/config';
 
 const requireAuth = (to, from, next) => {
-  let user = auth.currentUser;
-  if (!user) {
-    next({ name: 'Home' });
-  } else {
-    next();
-  }
-  next();
+	let user = auth.currentUser;
+	if (!user) {
+		next({ name: 'Home' });
+	} else {
+		next();
+	}
 };
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: HomeView,
-    },
-    {
-      path: '/chatroom',
-      name: 'Chatroom',
-      component: ChatroomView,
-      beforeEnter: requireAuth,
-    },
-  ],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'Home',
+			component: HomeView,
+		},
+		{
+			path: '/chatroom',
+			name: 'Chatroom',
+			component: ChatroomView,
+			beforeEnter: requireAuth,
+		},
+	],
 });
 
 export default router;

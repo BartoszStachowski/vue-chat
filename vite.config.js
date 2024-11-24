fe3f9019e868586d8e029929e8914405dce37c51
BartoszStachowski <stachowskibartosz96@gmail.com>
2024-11-24 14:24:52 +0100

    vie config to deploy

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
   base: '/vue-chat/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
